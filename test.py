ef5d875e19254807c9b4ef24ca5f3f4fcdb6601b
finnff <finn.fonteijn@student.hu.nl>
2021-12-15 21:34:16 +0000

    logs work now, sniffer was incorrectly installed.

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from spherov2 import scanner
 from spherov2.toy import mini
 from spherov2.sphero_edu import SpheroEduAPI
-import tty, sys, termios
+import tty
+import sys
+import termios
 
 FileDescriptor = termios.tcgetattr(sys.stdin)
 tty.setcbreak(sys.stdin)
@@ -14,35 +16,19 @@ print(f"Toy found: {toy.toy_type.display_name} - {toy.name}")
 print("Attempting to connect...")
 with SpheroEduAPI(toy) as api:
     print(f'Connected to {toy.name}')
-    api.spin(360,0.1)
+    api.spin(360, 0.1)
 
     a = True
     esc = 0
     print(f"Press {esc} to exit")
-    while a:
-        pressed = 0
-        pressed =sys.stdin.read(1)[0]
-        match pressed:
-            case "w":
-                api.roll(180, 255, 0.0001)
-                continue
-            case "a":
-                api.roll(90, 255, 0.00001)
-                continue
-            case "s":
-                api.roll(0, 255, 0.0001)
-                continue
-            case "d":
-                api.roll(270, 255, 0.0001)
-                continue
-            case esc:
-                api.stop_roll()
-                a = False
-                continue
-        
+    pressed = 0
+    pressed = sys.stdin.read(1)[0]
+    api.roll(180, 255, 0.0001)
+    api.roll(90, 255, 0.00001)
+    api.roll(0, 255, 0.0001)
+    api.roll(270, 255, 0.0001)
+
     print(f'Disconnected from toy {toy.name}')
-
-
 
 
 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, FileDescriptor)
